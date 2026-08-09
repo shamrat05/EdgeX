13587bcfce0e74b83e476d4e51021be3ed92ddca
0xFantasy <fcmfantasy1999@gmail.com>
2026-08-09 13:54:17 +0800

    style: enlarge media title text

--- app/src/main/java/com/fan/edgex/overlay/QuickSettingsPanelManager.kt
+++ app/src/main/java/com/fan/edgex/overlay/QuickSettingsPanelManager.kt
@@ -300,7 +300,7 @@ private class QuickSettingsPanelWindow(
                 orientation = LinearLayout.VERTICAL
                 mediaTitleView = TextView(context).apply {
                     setTextColor(Color.WHITE)
-                    textSize = 12f
+                    textSize = 13f
                     gravity = Gravity.CENTER
                     isSingleLine = true
                     ellipsize = TextUtils.TruncateAt.MARQUEE
